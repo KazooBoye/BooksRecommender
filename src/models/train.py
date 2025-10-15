aef3a76f553fdf0434d50430cf5035fac069a21d
Cao Duc Anh <ducanh127nps@gmail.com>
2025-10-15 08:40:06 +0700

    Small bugfix
    Fix missing import issue and corrected num_tags in config

--- src/models/train.py
+++ src/models/train.py
@@ -6,6 +6,7 @@ import os
 import json
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
